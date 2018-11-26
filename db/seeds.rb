4a172f9cd5e06a7423a844045aa0d5e86a2c8e10
Emmanuel Decle <emmanuel.decle@hotmail.fr>
2018-11-26 18:18:47 +0100

    Seeds générées

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+puts 'Creating 100 fake users...'
+100.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    second_name: Faker::Name.last_name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    password: Faker::Internet.password,
+    email: Faker::Internet.email
+
+)
+  user.save!
+end
+
+
+
+Animal.destroy_all
+
+puts 'Creating 30 fake animals...'
+30.times do
+  user = Animal.new(
+    name: Faker::Name.first_name,
+    category: Faker::Dog.breed,
+    price: Faker::Commerce.price,
+    user_id: User.all.sample.id
+
+    animal.save!
+
+puts 'Finished!'
+
