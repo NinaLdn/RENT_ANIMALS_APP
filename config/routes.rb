fecc21af854fa4c26483b3398345d548ca75a062
Vincent Brass <vincentbrass@yahoo.fr>
2018-11-27 12:29:01 +0100

    modified show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :animals do
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:show]
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
